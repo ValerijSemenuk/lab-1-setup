20bb89bb479f4bae01d8d95e82efec0aa7e5d123
ValeriiSemeniuk <valerii.s.semeniuk@oa.edu.ua>
2026-04-29 19:56:37 +0300

    Make base path conditional for Vercel

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lab-1-setup/',   // наприклад, '/lab1/' або '/lab-1-setup/'
-})
+  base: process.env.VERCEL ? '/' : '/lab-1-setup/',
+});
